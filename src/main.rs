55be5ff6d6239e3e45752bcf1a84014ef843ace3
d3z <g@d3z.me>
2024-11-08 10:35:33 +0000

    Add newline to output

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,5 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    print!("{}: command not found", input.trim());
+    print!("{}: command not found\n", input.trim());
 }
