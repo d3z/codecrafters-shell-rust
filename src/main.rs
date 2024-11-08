7bf9f1a882c52b4f7baa0297aa61d88a75af7cbf
d3z <g@d3z.me>
2024-11-08 12:40:08 +0000

    Change type command to search PATH for commands

--- src/main.rs
+++ src/main.rs
@@ -35,13 +35,26 @@ fn run_builtin(command: &str, args: &[&str]) {
         "exit" => std::process::exit(args.get(0).unwrap_or(&"0").parse().unwrap()),
         "echo" => println!("{}", args.join(" ")),
         "type" => run_type(args.get(0).unwrap_or(&"")),
-        _ => print!("{}: command not found\n", command),
+        _ => println!("{}: command not found", command),
     }
 }
+
 fn run_type(command: &str) {
     if BUILTINS.contains(&command) {
         println!("{} is a shell builtin", command);
     } else {
-        println!("{}: not found", command);
+        find_command_in_path(command);
     }
+}
+ 
+fn find_command_in_path(command: &str) {
+    let path = std::env::var("PATH").unwrap();
+    for dir in path.split(':') {
+        let full_path = format!("{}/{}", dir, command);
+        if std::path::Path::new(&full_path).exists() {
+            println!("{} is {}", command, full_path);
+            return;
+        }
+    }
+    println!("{}: not found", command);
 }
