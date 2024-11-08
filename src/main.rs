56ab37f43170143be62408b7c75c5588d436e009
d3z <g@d3z.me>
2024-11-08 12:27:23 +0000

    Implement the type command

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,32 @@ fn main() {
     }
 }
 
+const BUILTINS: &[&str] = &["exit", "echo", "type"];
+
 fn run_command(input: &str) {
     let args: Vec<&str> = input.split_whitespace().collect();
     let command = args[0];
     let args = &args[1..];
 
+    if BUILTINS.contains(&command) {
+        run_builtin(command, args);
+    } else {
+        println!("{}: command not found", command);
+    }
+}
+
+fn run_builtin(command: &str, args: &[&str]) {
     match command {
         "exit" => std::process::exit(args.get(0).unwrap_or(&"0").parse().unwrap()),
         "echo" => println!("{}", args.join(" ")),
+        "type" => run_type(args.get(0).unwrap_or(&"")),
         _ => print!("{}: command not found\n", command),
+    }
+}
+fn run_type(command: &str) {
+    if BUILTINS.contains(&command) {
+        println!("{} is a shell builtin", command);
+    } else {
+        println!("{}: not found", command);
     }
 }
